6b1f74b309847427250dfc5aab16198322187706
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2025-04-20 17:25:32 -0300

    Finalizei o slider

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+let imgSlider = document.querySelectorAll('.slider-container .slider-box');
+let btnProx = document.querySelector('#proxima');
+let btnAnter = document.querySelector('#anterior');
+let btnNav = document.querySelectorAll('.btn-nav-box .btn-nav');
+
+let contadorImg = imgSlider.length;
+let imgAtiva = 0;
+
+setInterval(() => {
+    imgAtiva++;
+    if (imgAtiva >= contadorImg) {
+        imgAtiva = 0;
+    }
+    mostrarSlider();
+}, 6000)
+
+btnProx.addEventListener('click', () => {
+    imgAtiva++;
+    if (imgAtiva >= contadorImg) {
+        imgAtiva = 0;
+    }
+    mostrarSlider();
+});
+
+btnAnter.addEventListener('click', () => {
+    imgAtiva--;
+    if (imgAtiva < 0) {
+        imgAtiva = contadorImg - 1;
+    }
+    mostrarSlider();
+});
+
+function mostrarSlider() {
+    let antigaImg = document.querySelector('.slider-container .slider-box.ativo');
+    let antigoBtnNav = document.querySelector('.btn-nav-box .btn-nav.ativo');
+
+    antigaImg.classList.remove('ativo');
+    antigoBtnNav.classList.remove('ativo');
+
+    imgSlider[imgAtiva].classList.add('ativo');
+    btnNav[imgAtiva].classList.add('ativo');
+}
+
+btnNav.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        imgAtiva = index;
+        mostrarSlider();
+    });
+});
+
+let rn = -1;
